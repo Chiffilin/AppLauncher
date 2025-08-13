9b47397cabd92143c1591c333879086245b795f5
chiffilin <chiffilin@gmail.com>
2025-08-13 14:17:09 +0300

    work version 0.1

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import sys
 
 from app.gui.main_window import app_start
-from app.logic.logger import log_exception
 
 
 def main():
     app_start()
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception:
-        # Перехват любых ошибок при старте
-        log_exception(*sys.exc_info())
+    main()
